950de78ca7cc566141bd9fe3913846ecf0695abc
Alan Willoughby <satyadvaya@gmail.com>
2020-03-30 17:52:41 -0700

    complete through Styles section of lab

---
+++ dani-is-yes.js
@@ -0,0 +1,14 @@
+function isYes(text) {
+    text = text.toLowerCase();
+    if (text === 'y') {
+        return true;
+    } else if (text === 'yes') {
+        return true;
+    } else if (text === 'yasss') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export default isYes;
